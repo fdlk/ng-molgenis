ef51f3d0df36e612c1e3eb234a9b46daf8256311
Fleur Kelpin <f.kelpin@umcg.nl>
2014-11-12 21:04:51 +0100

    mrefs

--- app/js/directives.js
+++ app/js/directives.js
@@ -45,6 +45,29 @@ angular.module('molgenis.directives', []).
 
                         }
                         break;
+                    case "MREF":
+                        scope.options=[];
+                        scope.controlType = "select";
+                        scope.operatorEditable = true;
+                        scope.criteria = [{selectedOperator:'AND'}];
+                        scope.plus = function(){
+                            scope.criteria[scope.criteria.length-1].operator = 'OR'
+                            scope.criteria.push({selectedOperator:'AND'});
+                        }
+                        scope.minus = function(index){
+                            if(index == scope.criteria.length - 1) {
+                                delete scope.criteria[index - 1].operator;
+                            }
+                            scope.criteria.splice(index, 1);
+
+                        }
+                        scope.$watch('$select.search', function(data){
+                            /*
+                             {"num":1000,"q":[{"operator":"NESTED","nestedRules":[{"field":"value","operator":"LIKE","value":"l"}]},{"operator":"OR"},{"operator":"NESTED","nestedRules":[{"field":"label","operator":"LIKE","value":"l"}]}]}
+                             */
+                            console.log(data);
+                            scope.options = ["label1", "label2"];
+                        });
                 }
             }
         };
